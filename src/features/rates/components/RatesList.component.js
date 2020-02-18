46c8ce4f99c4ec63db97f3e12b765f3216b31479
Abtin Ghods <abtin.ghods@gmail.com>
2020-02-18 12:17:10 -0500

    Creates rates list initial state UI

--- src/features/rates/components/RatesList.component.js
+++ src/features/rates/components/RatesList.component.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Flex, Box, Heading1, PrimaryButton } from '../../../design-system';
 
 export const RatesList = () => {
-  return <h2>Rates List</h2>;
+  return (
+    <Flex justifyContent="center" m={2} data-test="rates">
+      <Box width={4}>
+        <Flex flexDirection="column" height={2} mb={3}>
+          <Heading1>Exchange Rates</Heading1>
+        </Flex>
+
+        <PrimaryButton title="Load rates" />
+      </Box>
+    </Flex>
+  );
 };
